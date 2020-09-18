f5df39dc638886ddaa47d89bdd7f817a1354a3ca
99002496 <71483090+99002496@users.noreply.github.com>
2020-09-18 19:57:43 +0530

    Update test_properties_number.c

--- test/test_properties_number.c
+++ test/test_properties_number.c
@@ -1,47 +1,71 @@
-# Name of the project
-PROJECT_NAME = amstrong_number_check
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
-# Output directory
-BUILD = build
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "Calculator"
 
-# All source code files
-SRC = main.c\
-src/AmstrongNumber.c\
+/* Prototypes for all the test functions */
+void test_add(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
 
-# All test source files
-TEST_SRC = src/AmstrongNumber.c\
-test/test_AmstrongNumber.c
-
-TEST_OUTPUT = $(BUILD)/Test_$(PROJECT_NAME).out
-
-# All include folders with header files
-INC	= -Iinc
-
-PROJECT_OUTPUT = $(BUILD)/$(PROJECT_NAME).out
-
-# Default target built
-$(PROJECT_NAME):all
-
-# Run the target even if the matching name exists
-.PHONY: run clean test  doc all
-
-all: $(SRC) $(BUILD)
-	gcc $(SRC) $(INC) -o $(PROJECT_OUTPUT).out
-
-# Call `make run` to run the application
-run:$(PROJECT_NAME)
-	./$(PROJECT_OUTPUT).out
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "add", test_add);
+  CU_add_test(suite, "subtract", test_subtract);
+  CU_add_test(suite, "multiply", test_multiply);
+  CU_add_test(suite, "divide", test_divide);
 
 
-# Build and run the unit tests
-test:$(BUILD)
-	gcc $(TEST_SRC) $(INC) -o $(TEST_OUTPUT) -lcunit
-	./$(TEST_OUTPUT)
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
 
-# Remove all the built files, invoke by `make clean`
-clean:
-	rm -rf $(BUILD)
+/* Write all the test functions */ 
+void test_add(void) {
+  CU_ASSERT(30 == add(10, 20));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1500 == add(750, 7500));
+}
 
-# Create new build folder if not present
-$(BUILD):
-	mkdir build
+void test_subtract(void) {
+  CU_ASSERT(-3 == subtract(0, 3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == subtract(1000, 900));
+}
+
+void test_multiply(void) {
+  CU_ASSERT(0 == multiply(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == multiply(2, 5));
+}
+
+void test_divide(void) {
+  CU_ASSERT(0 == divide(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == divide(2, 2));
+}
