3ee8d32bc94ea44b7e38363c9e6fbb885117765d
99002496 <71483090+99002496@users.noreply.github.com>
2020-09-18 21:32:23 +0530

    Update test_properties_number.c

--- test/test_properties_number.c
+++ test/test_properties_number.c
@@ -1,11 +1,11 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 
-/* Modify these two lines according to the project */
+
 #include <properties_number.h>
 #define PROJECT_NAME    "Property_Analyzer"
 
-/* Prototypes for all the test functions */
+
 void test_prime(void);
 void test_armstrong(void);
 void test_even_odd(void);
@@ -19,17 +19,16 @@ void test_divisibility_3(void);
 void test_divisibility_5(void);
 void test_divisibility_7(void);
 
-/* Start of the application test */
+
 int main() {
-/* Note: Do not edit START*/
-  /*Initialize and setup the Test Framework */
+  
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-/* Note: Do not edit END */
+
   
   
-  /* Add your test functions in this format for testing*/
+ 
   CU_add_test(suite, "prime", test_prime);
   CU_add_test(suite, "armstrong", test_armstrong);
   CU_add_test(suite, "even_odd", test_even_odd);
@@ -43,24 +42,23 @@ int main() {
   CU_add_test(suite, "divisibility_5", test_divisibility_5);
   CU_add_test(suite, "divisibility_7", test_divisibility_7);
 
-/* Note: Do not edit START*/
-  /* Setup Test Framework to output the result to Screen */
+
   CU_basic_set_mode(CU_BRM_VERBOSE);
   
-  /* run the unit test framework*/
+  
   CU_basic_run_tests();
   
-  /* Cleaning the Resources used by Unit test framework */
+  
   CU_cleanup_registry();
-/* Note: Do not edit END */
+
   return 0;
 }
 
-/* Write all the test functions */ 
+ 
 void test_prime(void) {
   CU_ASSERT(1 == prime(1));
   
-  /* Dummy fail*/
+  
   CU_ASSERT(1 == prime(15));
 }
 
