44f33c0623f54418b3eb9e04c0e759ffb0772ef0
99002496 <71483090+99002496@users.noreply.github.com>
2020-09-19 12:32:47 +0530

    Update properties_number.c

--- 03_Implementation/src/properties_number.c
+++ 03_Implementation/src/properties_number.c
@@ -1,11 +1,7 @@
 #include "properties_number.h"
-int prime()
+int prime(int number)
 {
-    int flag=1, half, iteration,number ;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
+    int flag=1, half, iteration ;
     half=number/2;
     for(iteration=2;iteration<=half;iteration++)
     {
@@ -17,14 +13,9 @@ int prime()
     }
     return flag;
 }
-}
-int armstrong()
+int armstrong(int number)
 {
-   int reminder, sum=0, copy_number,flag,number;
-   printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
+   int reminder, sum=0, copy_number,flag;
    copy_number = number;
    while (number != 0)
    {
@@ -37,20 +28,13 @@ int armstrong()
        flag=1;
    }
    else
-   {
        flag=0;
-   }
+   
     return flag;
 }
-}
-int even_odd()
+int even_odd(int number)
 {
     int flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     if(number%2 == 0)
     {
         flag=1;
@@ -61,15 +45,9 @@ int even_odd()
     }
         return flag;
 }
-}
-int palindrome()
+int palindrome(int number)
 {
     int remainder, reversed_number=0, copy_number,flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
      copy_number=number;
      while (number != 0)
         {
@@ -87,15 +65,9 @@ int palindrome()
         }
         return flag;
 }
-}
-int power()
+int power(int number)
 {
     int flag=1, copy_number;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     copy_number = number;
      while(copy_number!=1)
     {
@@ -107,16 +79,10 @@ int power()
     }
     return flag;
 }
-}
 
-int harshad()
+int harshad(int number)
 {
     int reminder,sum=0,copy_number,flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     copy_number = number;
      while(number > 0)
         {
@@ -134,16 +100,10 @@ int harshad()
     }
     return flag;
 }
-}
 
-int perfect_square()
+int perfect_square(int number)
 {
     int iteration,flag=0;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     for(iteration=0;iteration<=number;iteration++)
     {
         if(number==iteration*iteration)
@@ -153,16 +113,10 @@ int perfect_square()
     }
     return flag;
 }
-}
 
-int perfect_cube()
+int perfect_cube(int number)
 {
     int iteration,flag=0;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     for(iteration=0;iteration<=number;iteration++)
     {
         if(number==iteration*iteration*iteration)
@@ -172,15 +126,9 @@ int perfect_cube()
     }
     return flag;
 }
-}
-int automorphic_number()
+int automorphic_number(int number)
 {
     int square, flag=1;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     square=number*number;
 while (number > 0)
     {
@@ -193,15 +141,10 @@ while (number > 0)
     }
     return flag;
 }
-}
-int divisibility_3()
+
+int divisibility_3(int number)
 {
     int flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-     if(number>0)
-    {
     if(number%3==0)
     {
        flag=1;
@@ -212,16 +155,10 @@ int divisibility_3()
     }
     return flag;
 }
-}
 
-int divisibility_5()
+int divisibility_5(int number)
 {
     int flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-    if(number>0)
-    {
     if(number%5==0)
     {
         flag=1;
@@ -232,16 +169,10 @@ int divisibility_5()
     }
     return flag;
 }
-}
 
-int divisibility_7()
+int divisibility_7(int number)
 {
     int flag;
-    int number;
-    printf("Enter your number to be checked\n");
-    scanf("%d", &number);
-    if(number>0)
-    {
     if(number%7==0)
     {
         flag=1;
@@ -251,51 +182,4 @@ int divisibility_7()
         flag=0;
     }
     return flag;
-    }
-}
-
-int operations()
-{
-    int calculator_operand1 = 0;
-    int calculator_operand2 = 0;
-    int addition, subtraction, multiplication, division, calculator_operation;
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n");
-    printf("Enter the associated number to perform operation\n");
-    scanf("%d", &calculator_operation);
-
-    switch(calculator_operation)
-    {
-        case 1:
-            printf("Enter two number\n");
-            scanf("%d %d", &calculator_operand1, &calculator_operand2);
-            printf("\n\t%d + %d = ", calculator_operand1, calculator_operand2);
-            addition=calculator_operand1+calculator_operand2;
-            return addition;
-            break;
-        case 2:
-            printf("Enter two number\n");
-            scanf("%d %d", &calculator_operand1, &calculator_operand2);
-            printf("\n\t%d + %d = ", calculator_operand1, calculator_operand2);
-            subtraction=calculator_operand1-calculator_operand2;
-            return subtraction;
-            break;
-        case 3:
-            printf("Enter two number\n");
-            scanf("%d %d", &calculator_operand1, &calculator_operand2);
-            printf("\n\t%d + %d = ", calculator_operand1, calculator_operand2);
-            multiplication=calculator_operand1 * calculator_operand2;
-            return multiplication;
-            break;
-        case 4:
-            printf("Enter two number\n");
-            scanf("%d %d", &calculator_operand1, &calculator_operand2);
-            printf("\n\t%d + %d = ", calculator_operand1, calculator_operand2);
-            division=calculator_operand1 / calculator_operand2;
-            return division;
-            break;
-        default:
-            printf("Exiting\n\n");
-            return NULL;
-}
 }
