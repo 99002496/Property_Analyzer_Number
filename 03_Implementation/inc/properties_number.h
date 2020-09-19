dbf30070b3332814aa6fdf796ea0fbdd334ad63f
99002496 <71483090+99002496@users.noreply.github.com>
2020-09-19 12:32:04 +0530

    Update properties_number.h

--- 03_Implementation/inc/properties_number.h
+++ 03_Implementation/inc/properties_number.h
@@ -1,20 +1,21 @@
+* @file properties_number.h
+*
+*/
 #ifndef __CALCULATOR_OPERATIONS_H__
 #define __CALCULATOR_OPERATIONS_H__
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int prime(void);
-int armstrong(void);
-int even_odd(void);
-int palindrome(void);
-int power(void);
-int harshad(void);
-int perfect_square(void);
-int perfect_cube(void);
-int automorphic_number(void);
-int divisibility_3(void);
-int divisibility_5(void);
-int divisibility_7(void);
-int operations(void);
+int prime(int number);
+int armstrong(int number);
+int even_odd(int number);
+int palindrome(int number);
+int power(int number);
+int harshad(int number);
+int perfect_square(int number);
+int perfect_cube(int number);
+int automorphic_number(int number);
+int divisibility_3(int number);
+int divisibility_5(int number);
+int divisibility_7(int number);
 #endif
-
